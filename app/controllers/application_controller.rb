1015ef91dc3dfe19079bd9300feb23bf6565f9de
shyamahira <shyamahira@bbtower.co.jp>
2018-05-21 17:50:39 +0900

    エラー用jsonデータ追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,27 @@ class ApplicationController < ActionController::Base
   def current_resource_owner
     @current_resource_owner ||= User.find(resource_owner_id) if resource_owner_id
   end
+
+  def doorkeeper_unauthorized_render_options(error: nil)
+    status_code, error_code, error_type = case error.status
+                              when :unauthorized
+                                [401, 40100, "Unauthorized"]
+                              when :forbidden
+                                [403, 40300, "Forbidden"]
+                              end
+
+    {
+      json: {
+        errors: [
+          {
+            message: error.description,
+            type: error_type,
+            code: error_code,
+            more_info: "",
+          }
+        ]
+      },
+      status: status_code
+    }
+  end
 end
